d8901f4b06c41feb16b44241e68b2af553586828
consueloramono <romadreams1@gmail.com>
2023-10-16 12:43:19 +0300

    5.1\ New test for addBook()

--- src/test/java/org/example/BookTest.java
+++ src/test/java/org/example/BookTest.java
@@ -2,42 +2,50 @@ package org.example;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import static org.junit.jupiter.api.Assertions.*;
+class BookLibraryTest {
 
-class BookTest {
+    @Test
+    void addBook() {
+        BookLibrary bookLibrary = new BookLibrary();
+        Book book = new Book("111", "111", "111", "111");
+        bookLibrary.addBook(book);
+        assertTrue(bookLibrary.findBook(book.name));
+    }
 
     @Test
     void showAllBooks() {
-        ArrayList<Book> books = new ArrayList<>();
-        books.add(new Book("111", "111", "111", "111"));
-        books.add(new Book("222", "222", "222", "222"));
-        books.add(new Book("333", "333", "333", "333"));
-        Book.showAllBooks(books);
+        BookLibrary bookLibrary = new BookLibrary();
+        bookLibrary.addBook(new Book("111", "111", "111", "111"));
+        bookLibrary.addBook(new Book("222", "222", "222", "222"));
+        bookLibrary.addBook(new Book("333", "333", "333", "333"));
+        bookLibrary.showAllBooks();
     }
 
     @Test
     void findBook() {
-        ArrayList<Book> books = new ArrayList<>();
-        books.add(new Book("111", "111", "111", "111"));
-        books.add(new Book("222", "222", "222", "222"));
-        books.add(new Book("333", "333", "333", "333"));
-        boolean result = Book.findBook(books, "123");
+        BookLibrary bookLibrary = new BookLibrary();
+        bookLibrary.addBook(new Book("111", "111", "111", "111"));
+        bookLibrary.addBook(new Book("222", "222", "222", "222"));
+        bookLibrary.addBook(new Book("333", "333", "333", "333"));
+
+        boolean result = bookLibrary.findBook("123");
         assertFalse(result);
 
-        result = Book.findBook(books, "111");
+        result = bookLibrary.findBook("111");
         assertTrue(result);
     }
 
     @Test
     void removeBook() {
-        ArrayList<Book> books = new ArrayList<>();
-        books.add(new Book("111", "111", "111", "111"));
-        books.add(new Book("222", "222", "222", "222"));
-        books.add(new Book("333", "333", "333", "333"));
-        assertTrue(Book.removeBook(books, "111"));
-        assertFalse(Book.removeBook(books, "123"));
-    }
+        BookLibrary bookLibrary = new BookLibrary();
+        bookLibrary.addBook(new Book("111", "111", "111", "111"));
+        bookLibrary.addBook(new Book("222", "222", "222", "222"));
+        bookLibrary.addBook(new Book("333", "333", "333", "333"));
 
+        assertTrue(bookLibrary.removeBook("111"));
+        assertFalse(bookLibrary.removeBook("123"));
+    }
 }
