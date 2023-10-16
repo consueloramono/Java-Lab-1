6a7ed1e6af94bb051a18aa5e053b05d8a95def4e
consueloramono <romadreams1@gmail.com>
2023-10-16 12:44:19 +0300

    5.3\ Edit and split code for Book.java & BookLibrary.java

--- src/main/java/org/example/Book.java
+++ src/main/java/org/example/Book.java
@@ -1,12 +1,11 @@
 package org.example;
 
-import java.util.ArrayList;
 
 public class Book {
-    private final String year;
-    private final String isbn;
-    private final String author;
-    private final String name;
+    public final String year;
+    public final String isbn;
+    public final String author;
+    public final String name;
 
     public Book(String name, String author, String isbn, String year) {
         this.name = name;
@@ -20,39 +19,6 @@ public class Book {
         System.out.println("Author: " + this.author);
         System.out.println("ISBN: " + this.isbn);
         System.out.println("Year: " + this.year);
-    }
-
-    public static void showAllBooks(ArrayList<Book> books) {
-        for (Book book : books) {
-            book.showBook();
-            System.out.println();
-        }
-    }
-    public static boolean findBook(ArrayList<Book> books, String BookName){
-        for(Book book : books){
-            if(book.name.equals(BookName)){
-                book.showBook();
-                return true;
-            }
-        }
-        System.out.println("Не знайдено книжки з такою назвою!");
-        return false;
-    }
-    public static boolean removeBook(ArrayList<Book> books, String Query){
-        int index = 0;
-        for(Book book : books){
-            if(book.isbn.equals(Query)){
-                break;
-            }
-            index++;
-        }
-        if(index == books.size()){
-            System.out.println("Не знайдено книжки з таким номером ISBN!");
-            return false;
-        }
-        books.remove(index);
-        System.out.println("Книжку з номером " + Query + " видалено!");
-        return true;
     }
 }
 
